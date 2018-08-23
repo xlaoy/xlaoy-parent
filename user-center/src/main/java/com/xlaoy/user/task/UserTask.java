40fe0e99ad815cb1fcbf3978183c8480e3386da6
zhangyijun <zhangyijun@zchz.com>
2018-08-23 19:17:29 +0800

    定时任务

--- user-center/src/main/java/com/xlaoy/user/task/UserTask.java
+++ user-center/src/main/java/com/xlaoy/user/task/UserTask.java
@@ -22,6 +22,6 @@ public class UserTask implements SecheduledTask {
 
     @Override
     public void execute(String parameters) {
-        logger.info("/////UserTask");
+        logger.info("/////UserTask:  " + parameters);
     }
 }
